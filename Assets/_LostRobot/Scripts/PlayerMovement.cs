f4d9bf98db77f1ec0a80c0832a4d6f911c8afcf2
Saeed <Seedxlss@gmail.com>
2021-10-17 22:11:46 +0400

    Fixed the smooth  rotation of the player

--- Assets/_LostRobot/Scripts/PlayerMovement.cs
+++ Assets/_LostRobot/Scripts/PlayerMovement.cs
@@ -22,7 +22,7 @@ public class PlayerMovement : MonoBehaviour
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Controller.Move(direction * speed * Time.deltaTime);
 
