cfa6a19586c935f3d8e4e9017091b750495c96dc
Satchee <s.shastree@student.saedubai.com>
2021-10-17 21:25:58 +0400

    Added camera movement
    -Rotates according to mouse movement
    -Added collision detection

--- Assets/_LostRobot/Scripts/Player/CameraController.cs
+++ Assets/_LostRobot/Scripts/Player/CameraController.cs
@@ -6,13 +6,61 @@ public class CameraController : MonoBehaviour
 {
     public Vector3 offset;
     public Transform player;
-    public float speed = 0.2f;
+    public float moveSpeed = 0.2f;
+    public Transform focus;
 
-    private void LateUpdate()
+    public float mouseSensitivity = 3f;
+    public float zoomLevel = 1f;
+
+    //collision
+    private RaycastHit hit;
+    float camDistance;
+    Vector3 cameraDirection;
+    public Vector2 camDistMinMax = new Vector2(0.5f, 5f);
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        cameraDirection = focus.transform.localPosition.normalized;
+        camDistance = camDistMinMax.y;
+    }
+
+    private void Update()
+    {
+        SmoothMovement();
+        Rotation();
+        CameraCollision();
+    }
+
+    private void SmoothMovement()
     {
         Vector3 position = player.position + offset;
-        Vector3 finalPosition = Vector3.Lerp(transform.position, position, speed);
-        transform.position = finalPosition;
+        Vector3 finalPosition = Vector3.Lerp(focus.transform.position, position, moveSpeed);
+        focus.transform.position = finalPosition;
+    }
+
+    void Rotation()
+    {
+        float mouseX = Input.GetAxis("Mouse Y") * mouseSensitivity/2;
+        float mouseY = Input.GetAxis("Mouse X") * mouseSensitivity;
+        var rotation = Quaternion.Euler(focus.transform.rotation.eulerAngles.x - mouseX, 
+                                        focus.transform.rotation.eulerAngles.y + mouseY, focus.transform.rotation.eulerAngles.z);
+        focus.transform.rotation = rotation;
         transform.LookAt(player);
     }
+
+    void CameraCollision()
+    {
+        Vector3 desiredCameraPosition = transform.TransformPoint(cameraDirection * camDistMinMax.y);
+        if (Physics.Linecast(focus.transform.position, desiredCameraPosition, out hit))
+        {
+            camDistance = Mathf.Clamp(hit.distance, camDistMinMax.x, camDistMinMax.y);
+        }
+        else
+        {
+            camDistance = camDistMinMax.y;
+        }
+        focus.localPosition = cameraDirection * camDistance;
+    }
+
 }
