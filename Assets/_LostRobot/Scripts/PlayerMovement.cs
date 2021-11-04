9bbb8a01499440033130e3275354a479373413ad
Chris van Wyk <like7coolmen@gmail.com>
2021-11-04 04:21:06 +0400

    Imported Player Blockoout & Initial Animations

--- Assets/_LostRobot/Scripts/PlayerMovement.cs
+++ Assets/_LostRobot/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController Controller;
+    public Animator anim;
     public float speed = 12f;
     public GameObject focus;
 
@@ -17,7 +18,10 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
+        
+
         Vector3 direction = new Vector3(horizontal, 0, vertical) .normalized;
+        anim.SetFloat("Vel", direction.magnitude);
 
         if (direction.magnitude >= 0.1f)
         {
