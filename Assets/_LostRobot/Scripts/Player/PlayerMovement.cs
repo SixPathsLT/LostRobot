b6da2ce3912132808ad59f678d521fc573d438b8
SixPathsLT <wavesgamingllc@gmail.com>
2021-11-14 14:51:50 +0400

    Fixed bug of player not moving based on camera direction

--- Assets/_LostRobot/Scripts/Player/PlayerMovement.cs
+++ Assets/_LostRobot/Scripts/Player/PlayerMovement.cs
@@ -19,17 +19,19 @@ public class PlayerMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
 
 
-        Vector3 direction = new Vector3(horizontal, 0, vertical) .normalized;
-        anim.SetFloat("Vel", direction.magnitude);
+        //Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        Vector3 direction = ((focus.transform.right * horizontal) + (focus.transform.forward * vertical)).normalized;
+        direction.y = 0;
+
+       anim.SetFloat("Vel", direction.magnitude);
 
         if (direction.magnitude >= 0.1f)
         {
-          
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+
+            float targetAngle = (Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg);
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-           
+            
             Controller.Move(direction * speed * Time.deltaTime);
 
         }
