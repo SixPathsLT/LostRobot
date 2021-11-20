cc7ed6bd938c63034cc0f198ce2dceb7734832ba
SixPathsLT <wavesgamingllc@gmail.com>
2021-11-21 02:02:12 +0400

    Added function to check if entity can see target
    - Utils class with helper functions to easily check if a transform should be able to detect another transform.

---
+++ Assets/_LostRobot/Scripts/Utilities/Utils.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public static class Utils {
+
+    public static bool CanSeeTransform(Transform viewer, Transform target, float maxDegrees = 45f, float maxDistance = 100f) {
+        float degree = Vector3.Angle(target.position - viewer.position, viewer.forward);
+        Vector3 targetDirection = (target.position - viewer.position);
+
+        if (degree < 90 && Vector3.Distance(viewer.position, target.position) < 3f)
+            return true;
+        
+        if (degree < maxDegrees) {
+            RaycastHit hit;
+            if (Physics.Raycast(viewer.position, targetDirection, out hit, maxDistance, 1, QueryTriggerInteraction.Ignore)) {
+                if (hit.collider.transform == target)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+
+}
