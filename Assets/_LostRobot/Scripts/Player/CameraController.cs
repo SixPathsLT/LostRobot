37f3dfb31746394f9cdcad2e4ee3e1d68ab4250b
SixPathsLT <wavesgamingllc@gmail.com>
2021-11-27 18:53:07 +0400

    Fixed jittery cam & cam going through walls/player

--- Assets/_LostRobot/Scripts/Player/CameraController.cs
+++ Assets/_LostRobot/Scripts/Player/CameraController.cs
@@ -41,7 +41,7 @@ public class CameraController : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse X") * mouseSensitivity;
       
         float xRot = rotation.eulerAngles.x - mouseX;
-        xRot = Mathf.Clamp(xRot, 2, 60);
+        xRot = Mathf.Clamp(xRot, 2, 40);
         rotation = Quaternion.Euler(xRot,
                                     rotation.eulerAngles.y + mouseY, rotation.eulerAngles.z);
         focus.transform.rotation = rotation;
@@ -61,40 +61,67 @@ public class CameraController : MonoBehaviour
             camDist = Mathf.Lerp(camDist, -zoomDist, Time.deltaTime * scrollDamp);
         }
     }
+
+    void ResetZoom() {
+        float maxDist = Mathf.Abs(camDist);
+
+        Vector3 checkVector = (cam.transform.position - focus.transform.position).normalized * maxDist;
+        checkVector.y = checkVector.y - 1;
+
+        if (Physics.Raycast(cam.transform.position, checkVector, out hit, maxDist)) {
+            float z = focus.transform.InverseTransformPoint(hit.point).z / 1.3f;
+            z = Mathf.Clamp(z, camDist, -1f);
+            Vector3 resetToVector = new Vector3(cam.transform.localPosition.x, 1.2f, z);
+
+            cam.transform.localPosition = Vector3.MoveTowards(cam.transform.localPosition, resetToVector, 5f * Time.deltaTime);
+        } else {
+            checkVector = (cam.transform.position - focus.transform.position).normalized * maxDist;
+
+            Vector3 resetToVector = new Vector3(cam.transform.localPosition.x, 1.2f, camDist);
+            if (Physics.Raycast(cam.transform.position, checkVector, out hit, maxDist)) {
+                float z = focus.transform.InverseTransformPoint(hit.point).z / 1.3f;
+                z = Mathf.Clamp(z, camDist, -1f);
+                resetToVector.z = z;
+            }
+            
+            cam.transform.localPosition = Vector3.MoveTowards(cam.transform.localPosition, resetToVector, 5f * Time.deltaTime);
+        }
+    }
     
-    private void CameraCollision()
-    {
-        if (cam.transform.localPosition.y < 1f)
+    private void CameraCollision() {
+
+       if (cam.transform.localPosition.y < 1f)
             cam.transform.localPosition = Vector3.MoveTowards(cam.transform.localPosition, new Vector3(cam.transform.localPosition.x, 1f, cam.transform.localPosition.z), 50f * Time.deltaTime);
 
+        // cam.transform.localPosition = Vector3.MoveTowards(cam.transform.localPosition, new Vector3(cam.transform.localPosition.x, 1f, camDist), 5f * Time.deltaTime);
 
-        Vector3 resetToVector = new Vector3(cam.transform.localPosition.x, 1f, camDist);
-        if (Physics.Raycast(cam.transform.position, cam.transform.position - focus.transform.position, out hit, Mathf.Abs(cam.transform.localPosition.z != camDist ?  camDist * 2 : camDist))) {
-            float z = focus.transform.InverseTransformPoint(hit.point).z / 2f;
-                resetToVector.z = z;
-        }
-        cam.transform.localPosition = Vector3.MoveTowards(cam.transform.localPosition, resetToVector, 5f * Time.deltaTime);
-
-        if (Physics.Linecast(focus.transform.position, cam.transform.position, out hit))
-        {
+        if (cam.transform.localPosition.z != -1 && Physics.Linecast(focus.transform.position, cam.transform.position, out hit)) {
             if (hit.collider.gameObject.CompareTag("Player"))
-                return; 
+                return;
 
-            Vector3 hitVector = new Vector3(hit.point.x, 1f, hit.point.z);
+            Vector3 hitVector = new Vector3(hit.point.x, hit.point.y, hit.point.z);
             //cam.transform.position = hitVector;
-             cam.transform.position = Vector3.MoveTowards(cam.transform.position, hitVector, 10f * Time.deltaTime);
+           // cam.transform.position = Vector3.MoveTowards(cam.transform.position, hitVector, 10f * Time.deltaTime);
 
             var desiredCamPos = cam.transform.localPosition;
-            desiredCamPos = new Vector3(desiredCamPos.x, 1f, desiredCamPos.z + collisionSensitivity);
+          //  desiredCamPos = new Vector3(desiredCamPos.x, desiredCamPos.y, desiredCamPos.z + collisionSensitivity);
+            float z = focus.transform.InverseTransformPoint(hit.point).z / 1.3f;
+            z = Mathf.Clamp(z, camDist, -1f);
+            desiredCamPos = new Vector3(desiredCamPos.x, desiredCamPos.y, z);
+
             //cam.transform.localPosition = desiredCamPos;
-            cam.transform.localPosition = Vector3.MoveTowards(cam.transform.localPosition, desiredCamPos, 10f * Time.deltaTime);
-        }
+
+            float dist = Vector3.Distance(hit.point, cam.transform.position) * 4f;
+            if (hit.collider.name.Contains("Wall"))
+                dist *= dist * 10f;
+
+           cam.transform.localPosition = Vector3.MoveTowards(cam.transform.localPosition, desiredCamPos, dist * Time.deltaTime);
+        } else if (!Physics.CheckSphere(cam.transform.position, 0.5f, 1, QueryTriggerInteraction.Ignore))
+            ResetZoom();
+        
         if (cam.transform.localPosition.z > -1f)
-        {
            // cam.transform.localPosition = new Vector3(cam.transform.localPosition.x, cam.transform.localPosition.y, -1f);
             cam.transform.localPosition = Vector3.MoveTowards(cam.transform.localPosition, new Vector3(cam.transform.localPosition.x, 1f, -1f), 5f * Time.deltaTime);
-       
-        }
 
 
     }
