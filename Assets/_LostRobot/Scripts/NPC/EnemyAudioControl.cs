bb2ee0990d42513909fce092a855c3e870b47bc9
Like7 <like7coolmen@gmail.com>
2021-12-14 20:55:32 +0400

    Added Enemy Sounds & Adjusted levels for minimap functionality

---
+++ Assets/_LostRobot/Scripts/NPC/EnemyAudioControl.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAudioControl : MonoBehaviour
+{
+
+    public AudioClip[] clips;
+    private AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void Step()
+    {
+        AudioClip clip = clips[Random.Range(0,3)];
+        source.volume = .4f;
+        RandomizePitch(.5f, 1f);
+        source.PlayOneShot(clip);
+    }
+
+    void RandomizePitch(float min, float max)
+    {
+        source.pitch = Random.Range(min, max);
+    }
+
+    void Investigate()
+    {
+        source.volume = .4f;
+        AudioClip clip = clips[3];
+        source.PlayOneShot(clip);
+    }
+
+    public void Found()
+    {
+        source.volume = .4f;
+        AudioClip clip = clips[4];
+        source.PlayOneShot(clip);
+    }
+
+    public void Stun()
+    {
+        source.volume = .4f;
+        AudioClip clip = clips[5];
+        source.PlayOneShot(clip);
+    }
+}
