07c82cb151eef174f7552b102756d0308d72bbfb
SixPathsLT <wavesgamingllc@gmail.com>
2021-12-17 16:17:48 +0400

    Improvements/Additions & Fixes to Lockdown
    - Made it so relevant facility lights will glow redish while lockdown is in place.
    - Added fading of lockdown narrator voice when a lockdown is over.
    - Facility lockdown lights color will smoothly change to the appropriate colors.
    - Fixed lockdown music not playing

--- Assets/_LostRobot/Scripts/World/LockDown.cs
+++ Assets/_LostRobot/Scripts/World/LockDown.cs
@@ -9,40 +9,65 @@ public class LockDown : MonoBehaviour
     public static bool LockDownInitiated = false;
     private bool stopped = false;
 
+    AudioSource source;
     DoorManager doorManager;
-    
+    public Color originalColor;
+    public Material mat;
     private void Start() {
         StartCoroutine(LockDownCoroutine());
         doorManager = FindObjectOfType<DoorManager>();
+        mat.SetColor("_EmissionColor", Color.white * 1.1f);
+        source = GetComponent<AudioSource>();
     }
-    
+
+    float timePassed;
+    Color lerpedColor = Color.white;
+    bool red;
     private void Update()
     {
         /*if (LockDownInitiated && !playing)
             GetComponent<AudioSource>().Play();
         else if (!LockDownInitiated && playing)
             GetComponent<AudioSource>().Stop();*/
-
-        if (LockDownInitiated && GameManager.GetInstance().InPausedState() || GetComponent<AudioSource>().isPlaying)
-        {
-            GetComponent<AudioSource>().Stop();
+            
+        if (LockDownInitiated && GameManager.GetInstance().InPausedState() && source.isPlaying) {
+            source.Stop();
             stopped = true;
         }
 
-        if (LockDownInitiated && stopped && GameManager.GetInstance().InPlayingState())
-        {
-            GetComponent<AudioSource>().Play();
+        if (LockDownInitiated && stopped && GameManager.GetInstance().InPlayingState()) {
+            source.Play();
             stopped = false;
         }
 
-       
-        
+        timePassed += Time.deltaTime;
+        if (timePassed > (red || !LockDownInitiated ? 2 : 1)) {
+            red = !red;
+            timePassed = 0;
+        }
+
+        if (LockDownInitiated) {
+            if (red) {
+                lerpedColor = Color.Lerp(lerpedColor, Color.red, timePassed * Time.deltaTime * 2.5f);
+                mat.SetColor("_EmissionColor", lerpedColor * 2f);
+            } else {
+                lerpedColor = Color.Lerp(lerpedColor, Color.gray, timePassed * Time.deltaTime);
+                mat.SetColor("_EmissionColor", lerpedColor * 2f);
+            }
+        } else if (lerpedColor !=  Color.white) {
+            lerpedColor = Color.Lerp(lerpedColor, Color.white, timePassed * Time.deltaTime);
+            mat.SetColor("_EmissionColor", lerpedColor * 1.1f);
+            if (source.isPlaying) {
+                float volume = source.volume - (Time.deltaTime / 4);
+                source.volume = volume < 0 ? 0 : volume;
+            }
+        }
+
 
     }
 
-    private void CancelLockDown()
-    {
-        GetComponent<AudioSource>().loop = false;
+    private void CancelLockDown() {
+        source.loop = false;
         LockDownInitiated = false;
         stopped = false;
         doorManager.LockDownExit();
@@ -53,10 +78,10 @@ public class LockDown : MonoBehaviour
         var timer = Random.Range(intervalRange.x, intervalRange.y);
         yield return new WaitForSeconds(timer);
 
-        if (GameManager.GetInstance().InPlayingState())
-        {
-            GetComponent<AudioSource>().loop = true;
-            GetComponent<AudioSource>().Play();
+        if (GameManager.GetInstance().InPlayingState()) {
+            source.volume = 0.5f;
+            source.loop = true;
+            source.Play();
             LockDownInitiated = true;
 
             doorManager.LockDownEnter();
